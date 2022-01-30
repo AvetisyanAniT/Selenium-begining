01bc2b3f9d6a2dcffb00926b8bb722f32865ddaf
Ani <73894055+AvetisyanAniT@users.noreply.github.com>
2022-01-30 18:44:47 +0400

    Add files via upload

---
+++ SeleniumEasy_Anna/testdata.py
@@ -0,0 +1,11 @@
+data={   
+    "url": "https://www.seleniumeasy.com/test/",
+    "mainmenu": "Input Forms",
+    "submenu": "Radio Buttons Demo",
+
+    "radiobutton1": "Male"
+    "radiobutton2": "Female"
+ 
+}
+
+
