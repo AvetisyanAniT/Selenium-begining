81fe43dd48a4840cef351dfb40f1f00bbe27a868
Ani <73894055+AvetisyanAniT@users.noreply.github.com>
2022-01-30 18:41:02 +0400

    Delete Pages directory

--- Pages/Base_page.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-class TestBase:
-
-    def test_browser(self):
-        browser = webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application/chromedriver.exe')
-        return browser
